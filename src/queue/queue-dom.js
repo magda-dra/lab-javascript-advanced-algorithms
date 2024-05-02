5161680b911953b11481ecba27c8414db463cd24
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-05-02 14:50:03 +0100

    "Queue DOM structure changed - still not working"

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -10,38 +10,72 @@ const dequeue = document.querySelector('.btn-take-dequeue');
 const queue = new Queue();
 
 const clearQueueInput = () => {
-  // ... your code goes here
+  queueInput.value = '';
 };
 
 const generateListQueue = () => {
-  // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = queue.display().length;
+  let size = queue.MAX_SIZE - length;
+  queue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item;
+    stackList.appendChild(li);
+  });
+  for (let i=0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    stackList.appendChild(li);
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
-    // ... your code goes here
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
+
   } else if (type === 'overflow') {
-    // ... your code goes here
+    warningTopQueue.style.display = 'block'
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
-  try {
-    // ... your code goes here
-  } catch (error) {
-    // there was an overflow error, handle it
+  if(queue.push(queueInput.value) === 'Queue Overflow') {
+    generateWarningQueue('overflow');
+  } else {
+    clearQueueInput();
+    generateListQueue();
   }
 };
+/*   try {
+    clearQueueInput();
+    generateListQueue();
+  } catch (error) {
+    generateWarningQueue('overflow')
+  } */
+
 
 const removeFromQueue = () => {
-  try {
-    // ... your code goes here
-  } catch (error) {
-    // there was an underflow error, handle it
+  if(queue.pop() === 'Queue Underflow'){
+    generateWarningQueue('underflow');
+  } else {
+    generateListQueue();
   }
 };
+/*   try {
+    generateListQueue();
+  } catch (error) {
+    generateWarningQueue('underflow')
+  } */
+
+
 
 addQueue.addEventListener('click', addToQueue);
 dequeue.addEventListener('click', removeFromQueue);
